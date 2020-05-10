a87b98a786daf1937fc93e04011f14088c23a63a
Lourran Zorzi <lourran-zorzi@lourran.zorzi>
2020-05-10 14:16:41 -0300

    adicionando novo projeto com restify

---
+++ javascript+restfy/index.js
@@ -0,0 +1,9 @@
+const restify = require('restify');
+const server = restify.createServer()
+
+server.get('/hello', (req, res, next) => {
+    res.send('hello')
+    next()
+})
+
+server.listen(9191)
