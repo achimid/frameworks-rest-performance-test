91871673e48e01b60486f6e229253e6b3d5bdb34
Lourran Zorzi <lourran-zorzi@lourran.zorzi>
2021-04-25 22:14:36 -0300

    Ajustando script do readme

--- performance-test-script.sh
+++ performance-test-script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 
 connections=125
-number_of_requests=1000
+number_of_requests=1000000
 
 declare -a projects=(
     'java-springboot-jetty' 
@@ -17,7 +17,7 @@ for container_name in ${projects[@]}; do
     printf "===================== ${container_name} ======================\n\n"
 
     printf "=== Pull and Run docker application ===\n\n"
-    docker run -d --rm --net=host -p 8080:8080 --name ${container_name} --rm achimid/frameworks-performance-${container_name}:latest; sleep 5;
+    docker run -d --rm --net=host -p 8080:8080 --name ${container_name} --rm achimid/frameworks-performance-${container_name}:latest; sleep 10;
 
     printf "=== Running performance test ===\n\n"
     docker run --net=host -it alpine/bombardier -c ${connections} -n ${number_of_requests} -l localhost:8080/hello;
